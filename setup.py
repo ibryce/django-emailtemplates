806d40295cae7a50efe4f37eda1b710a60a8d4f5
Wiggins <wiggins@concentricsky.com>
2010-08-13 10:00:00 -0700

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-emailtemplates',
-    version='0.9.1',
+    version='0.9.2',
     packages = find_packages(),
 
     author = 'Concentric Sky',
